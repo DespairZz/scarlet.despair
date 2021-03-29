d0e370362839d24cbf8cad250e52340b1402a8fc
Edward <80621657+DespairZz@users.noreply.github.com>
2021-03-29 11:45:26 +0500

    v5

---
+++ assets/backgrounds.js
@@ -0,0 +1,7 @@
+let backgroundPic=Math.round(Math.random()*3)
+            let image = [];
+                image[0]="img/backgrounds/bg0.png";
+                image[1]="img/backgrounds/bg1.png";
+                image[2]="img/backgrounds/bg2.png";
+                image[3]="img/backgrounds/bg3.png";
+            document.write (" <img src="+image[backgroundPic]+"> ");
